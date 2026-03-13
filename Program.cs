d5cfae15fb06445f309bb626de01fd8cb5294546
Juan De Dios De La Luz Lozano Martinez <juan_lozano@cpm.coop>
2026-03-13 11:18:25 -0600

    Cambios

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Json("World JDD"));
+app.MapGet("/", () => Results.Json("World JDD Docker"));
 
 app.Run();
